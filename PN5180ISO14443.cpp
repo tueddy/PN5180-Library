b4afb5cdebf40bd71e1f59b598f939025cda387d
mjmeans <mjmeans@ultrasw.com>
2024-10-28 12:12:10 -0700

    turn off debug

--- PN5180ISO14443.cpp
+++ PN5180ISO14443.cpp
@@ -16,7 +16,7 @@
 // MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
 // Lesser General Public License for more details.
 //
-#define DEBUG 1
+//#define DEBUG 1
 
 #include <Arduino.h>
 #include "PN5180ISO14443.h"
